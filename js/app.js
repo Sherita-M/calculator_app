933b3489eeac828cf81e9263578627ec0628d535
Sherita Manning <smanningsn@gmail.com>
2023-04-01 17:36:38 -0700

    Updated index.html

--- js/app.js
+++ js/app.js
@@ -1,17 +1,8 @@
 /** @format */
 
-const displayArea = document.getElementById("main-display");
-const keypad = document.querySelectorAll("input");
+const 
 
-console.log(keypad);
 
-function getValue() {
-  let value = (document.getElementById("display").value = "");
-  displayArea.innerHTML = value;
-  return;
-}
-
-keypad.addEventListener("click", getValue);
 
 // Get input value on click
 // Run function that adds the input value to the main-display
