36b45061bae46d8fcbfeaf25e578d65610db0e96
Sherita Manning <84292756+Sherita-M@users.noreply.github.com>
2023-06-18 20:58:00 -0700

    Updated css: adjusted the zero  input button width

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 /** @format */
 
-const 
+const buttons = document.querySelectorAll(".button");
 
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonValue = button.textContent;
+  });
+});
 
+console.log(buttons);
 
 // Get input value on click
 // Run function that adds the input value to the main-display
